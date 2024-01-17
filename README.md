0752bb92627f0fae073cc58dad2ba1fde1f67d96
Geras882 <luisgoncalvesfigueira@gmail.com>
2024-01-17 18:12:04 +0000

    Change the images of the food choices to buttons. Add a event lister to the buttons so after a click they are added to the meal section. Add mealCalories function and refactor the calculate calories function

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ One of the most difficult things about doing a diet is to make a meal that match
 The page is going to be divided into two sections. The first one is a form that will ask the user their weight, height, physical activity and age so it can calculate the amount of calories needed daily, then calculate the amount of calories needed to generate caloric deficits making them lose weight. The second section will be the meal generator, there is going to be two boxes one will be the food box were all the food options are going to be displayed, represented as buttons with a click event listener, and the other box is going to be the meal box were once the food is clicked from the food box is going to appear on this box and underneath is going to have a calorie calculator telling the user how many calories this meal will have. The calories of the meal are going to be compared with the result of the meal calories made in the first section, in the case that is surpassed is going to show a message warning the user that it surpasses the number of calories and suggests removing one of the ingredients. 
 
 For propuses of this project, I decided to pick 10 food choices with the amount in grams or portions depending on the product. 
+
+## Credits
+
+love math code to add the event listeners to the food choices wen the page loads and increment score
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,75 @@
-function calculateCalories() {
-    let weight = parseFloat(document.getElementById("weight").value);
-    let height = parseFloat(document.getElementById("height").value);
-    let age = parseInt(document.getElementById("age").value);
-    let sex = document.getElementById("sex").value;
-    let dailyCalories = document.getElementById("daily-calories");
-    let caloricDeficit = document.getElementById("caloric-deficit");
-    let caloriesPerMeal = document.getElementById("calories-per-meal")
-    
-    let manCalculation = (10 * weight) + (6.25 * height) - (5 * age) + 5;
-    let womanCalculation = (10 * weight) + (6.25 * height) - (5 * age) - 161;
-    let calculateCaloricDeficitMan = manCalculation - (manCalculation * 0.2);
-    let calculateCaloricDeficitManMeal = calculateCaloricDeficitMan * 0.3;
-    let calculateCaloricDeficitWoman = womanCalculation - (womanCalculation * 0.2);
-    let calculateCaloricDeficitWomanMeal = calculateCaloricDeficitWoman * 0.3;
+/**
+ * This section adds the event listener
+ * to all food choices making them 
+ * appear on the choiceMeal section
+ */
 
-    if (sex == "man") {
-       dailyCalories.innerHTML = `
-       <p>${manCalculation}</p>
-       `
-       caloricDeficit.innerHTML = `
-       <p>${calculateCaloricDeficitMan}</p>
-       `
-       caloriesPerMeal.innerHTML = `
-       <p>${calculateCaloricDeficitManMeal}</p>
-       `
-    } else {
-       dailyCalories.innerHTML = `
-       <p>${womanCalculation}</p>
-       `
-       caloricDeficit.innerHTML = `
-       <p>${calculateCaloricDeficitWoman}</p>
-       `
-       caloriesPerMeal.innerHTML = `
-       <p>${calculateCaloricDeficitWomanMeal}</p>
-       `
-    }
+document.addEventListener("DOMContentLoaded", function() {
+   let foodButtons = document.getElementsByClassName('food-choise');
+   let choicedMeal = document.getElementById('choiced-meal');
+
+   for (let button of foodButtons) {
+      button.addEventListener("click", function() {
+         // Create a new image element
+         let newImage = document.createElement('img');
+         newImage.src = this.getAttribute('src');
+
+         // Append the new image to the existing content
+         choicedMeal.appendChild(newImage);
+         console.log(parseInt(this.getAttribute('value')));
+         mealCaloriesCounter(parseInt(this.getAttribute('value')))
+      })
+   }
+
+})
+
+/**
+ * mealCaloriesCounter is going to take the calories value
+ * everytime that a food is choiced and then is goin to 
+ * add the result to the in the total-meal-calories span
+ */
+function mealCaloriesCounter(num) {
+   let totalMealCalories = parseInt(document.getElementById('total-meal-calories').innerText);
+   document.getElementById('total-meal-calories').innerHTML = totalMealCalories + num
+
 }
 
+/**
+ * Function to calculate the amounth of calories, 
+ * caloric deficit and calories per meal
+ */
+
+function calculateCalories() {
+   //Create variables for each element
+   let weight = parseFloat(document.getElementById("weight").value);
+   let height = parseFloat(document.getElementById("height").value);
+   let age = parseInt(document.getElementById("age").value);
+   let sex = document.getElementById("sex").value; 
+   let dailyCalories = document.getElementById("daily-calories")
+   
+   //Calculation of the daily calories for man and woman
+   let manCalculation = (10 * weight) + (6.25 * height) - (5 * age) + 5;
+   let womanCalculation = (10 * weight) + (6.25 * height) - (5 * age) - 161;
+   let calculateCaloricDeficitMan = manCalculation - (manCalculation * 0.2);
+   let calculateCaloricDeficitManMeal = calculateCaloricDeficitMan * 0.3;
+   let calculateCaloricDeficitWoman = womanCalculation - (womanCalculation * 0.2);
+   let calculateCaloricDeficitWomanMeal = calculateCaloricDeficitWoman * 0.3;
+
+   if (sex == "man") {
+      dailyCalories.innerHTML = manCalculation
+      console.log(dailyCalories.innerText)
+      calculateCaloricDeficit(dailyCalories.innerText)
+   } else {
+      dailyCalories = womanCalculation
+      calculateCaloricDeficit(dailyCalories.innerText)
+   }
+}
+
+/**
+ * Calculation of the caloric deficit
+ */
+
+function calculateCaloricDeficit(num) {
+   let calculate = num - (num * 0.2);
+   document.getElementById("caloric-deficit").innerHTML = calculate;
+}
